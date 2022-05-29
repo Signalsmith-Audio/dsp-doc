03c79fc9f3fea077d92686df4bb503a6b8a595a7
Geraint <luffgd@gmail.com>
2022-05-29 08:13:54 +0100

    Generate docs for each tag, and assemble version notes

---
+++ historical-docs.sh
@@ -0,0 +1,26 @@
+printf "" > version-notes.txt
+
+current=$(git symbolic-ref --short HEAD)
+
+for tag in $(git tag -l "v*" --sort=version:refname)
+do
+	printf "$tag: " >> version-notes.txt
+	git log -n 1 --format="[%cs] %s" "$tag" -- >> version-notes.txt
+	git log -n 1 --format="%b" "$tag" -- | sed  's/^/     /' >> version-notes.txt
+     
+	if [ -d "$tag" ]
+	then
+		echo "Already exists: $tag"
+	else
+		echo "Tag: $tag"
+		git -c advice.detachedHead=false checkout "$tag"
+		pushd ..
+		git -c advice.detachedHead=false checkout "$tag";
+		popd
+
+		make clean test doxygen
+
+		mv html "$tag"
+	fi
+done
+git checkout "$current"
