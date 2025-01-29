8407e7fb76a6a0e9daecf1e07ede98905ecf4d60
Geraint Luff <geraint@signalsmith-audio.co.uk>
2025-01-29 09:59:18 +0000

    Release v1.6.1

--- tests/common/version.cpp
+++ tests/common/version.cpp
@@ -1,6 +1,6 @@
 #include "delay.h" // Anything that includes "common.h" should do
 
-SIGNALSMITH_DSP_VERSION_CHECK(1, 6, 0)
+SIGNALSMITH_DSP_VERSION_CHECK(1, 6, 1)
 
 // Test with some compatible earlier versions
 SIGNALSMITH_DSP_VERSION_CHECK(
